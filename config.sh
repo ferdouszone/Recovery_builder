8b4b3061c9ff84c9f01340f72c0c9ab463f83a90
ferdouszone <114679830+ferdouszone@users.noreply.github.com>
2023-10-09 22:48:11 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export NAME=twrp
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git
+export BRANCH=twrp-9.0
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DEVICE="ice" # codename used in device tree
+export DT_LINK="https://github.com/ferdouszone/android_device_xiaomi_ice" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export VENDOR="redmi" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
